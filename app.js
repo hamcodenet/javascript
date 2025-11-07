67d2b35e3b2f3d40bf5915fd7ec7bdcd8f7372c1
Hamid Haghdoost <tuytoosh@yahoo.com>
2025-11-08 00:24:41 +0100

    S3 - IF

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-let a = 12
-let b = '14'
 
-console.log(a + b)
+
+// < > <= >= == != 
+// && || !
+
+// let age = 12;
+// let gender = true;
+
+// if (age >= 18 && gender == true) {
+//     console.log("You have to do militry service first!")
+// }
+
+// productPrice = 100
+
+// userCredit = 150
+// userPremium = false
+
+// userBlocked = false
+
+// if ((userCredit > productPrice || userPremium) && !userBlocked) {
+//     console.log("You bought the product! Congrats...")
+// }
+
